99d1fb9fc83647344f97a632181c6b118ed177e7
hedisnice <nicenice60225@gmail.com>
2023-11-22 12:59:06 +0700

    feat(Exercise) add complete exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+let restaurantName = "Chef's Table by Chef James";
+let netBillAmount = 15000;
+let numberOfOpenRestaurants = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-5329 2400 County Rd #4 Meeker, Colorado(CO), 81641";
+netBillAmount = 5000;
+numberOfOpenRestaurants = 2500;
