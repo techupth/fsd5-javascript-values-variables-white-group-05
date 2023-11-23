fb6d7ef3d8d550b8b3e8a3dcea86c9a4629cbaa3
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-11-23 13:03:35 +0700

    feat(exercise): add completed exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let fname = "Karter";
+let lname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restuarantName = "Chef's Table by Chef James";
+const totalBillPrice = 15000;
+const OpenRestuarantNumber = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBillPrice = 5000;
+OpenRestuarantNumber = 2500;
